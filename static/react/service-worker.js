ac0f6b66c2ee962166a8d685eb6a01c079226e83
dvijaywargiya <dvijaywargiya@gmail.com>
2020-09-15 20:06:07 +0530

    b

--- static/react/service-worker.js
+++ static/react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/static/react/precache-manifest.b9989b473f1c55fd4b8c5d1583381e4b.js"
+  "/static/react/precache-manifest.c6988217a47276e4d0423eff3db98f8a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/static/react/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
